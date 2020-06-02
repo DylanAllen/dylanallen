ef7ce2190475a52b3df1dce2b8331e7eb9cdfa7e
Dylan <dylan.allen@voicefoundry.com>
2020-06-02 16:44:26 -0500

    blog update

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,50 +5,38 @@ import { meta as P2 } from './second';
 import { meta as P3 } from './third';
 import { MetaType } from '../../interfaces';
 import Link from '../../components/Link';
-import { getImage } from '../../utils/firebase';
-import { useState, useEffect } from 'react';
+import Picture from '../../components/Picture';
 
 let posts: MetaType[] = [
-    P1,
-    P2,
-    P3
+  P1,
+  P2,
+  P3
 ]
 
-const PostCard: React.FunctionComponent<{post: MetaType}> = ({ post }) => {
-        
-    const [url, setImg] = useState('');
-    
-    useEffect(() => {
-        if (post.image) {
-            (async () => {
-                const img = await getImage(post.image);
-                setImg(img);
-            })
-        }
-    },[]);
+const PostCard: React.FunctionComponent<{ post: MetaType }> = ({ post }) => {
 
-    return (
-        <article className="post-summary">
-            <Heading level={3}>{post.title}</Heading>
-            <Paragraph>{post.description}</Paragraph>
-            { url && <Image fit="cover" src={url} /> }
-            <Link path={`/blog/${post.slug}`}>View Post</Link>
-        </article>
-    )
+  return (
+    <article className="post-summary">
+      <Heading level={3}>{post.title}</Heading>
+      <Paragraph>{post.description}</Paragraph>
+      <Picture fbpath={post.image} style={{height: "300px", width: "300px"}} />
+      <Link path={`/blog/${post.slug}`}>View Post</Link>
+    </article>
+  )
 }
 
 const Blog: React.FunctionComponent = () => {
 
 
 
-    return (
-        <Layout title="Blog | Dylan Allen | JavaScript Developer | Frontend Web">
-            <Heading>Blog</Heading>  
-            <Box className="post-list">
-                {posts.map(post => <PostCard post={post} key={post.slug}></PostCard>)}
-            </Box>
-        </Layout>
-    )
+  return (
+    <Layout title="Blog | Dylan Allen | JavaScript Developer | Frontend Web">
+      <Heading>Blog</Heading>
+      <Box className="post-list">
+        {posts.map(post => <PostCard post={post} key={post.slug}></PostCard>)}
+      </Box>
+    </Layout>
+  )
 }
 
 export default Blog
